78f5b7899ea9bc7f2df10753c5932ca908fb1145
jess-izuu <izumijessica@gmail.com>
2020-10-12 12:22:48 -0400

    post Tweet

--- Twitter/TweetViewController.swift
+++ Twitter/TweetViewController.swift
@@ -10,10 +10,11 @@ import UIKit
 
 class TweetViewController: UIViewController {
 
+    @IBOutlet weak var tweetTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tweetTextView.becomeFirstResponder()
     }
     
     @IBAction func cancel(_ sender: Any) {
@@ -21,7 +22,14 @@ class TweetViewController: UIViewController {
     }
     
     @IBAction func tweet(_ sender: Any) {
-    }
+        if (!tweetTextView.text.isEmpty) {
+            TwitterAPICaller.client?.postTweet(tweetString: tweetTextView.text, success: { self.dismiss(animated: true, completion: nil) }, failure: { (error) in print("Error posting tweet \(error)")
+                self.dismiss(animated: true, completion: nil)
+        })
+        } else {
+            self.dismiss(animated: true, completion: nil)
+        }
+}
     
     /*
     // MARK: - Navigation

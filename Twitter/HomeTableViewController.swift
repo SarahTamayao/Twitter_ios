c773475724b2cabcd1f516a365674106632d2002
jess-izuu <izumijessica@gmail.com>
2020-10-16 14:26:21 -0400

    bool

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -93,7 +93,7 @@ class HomeTableViewController: UITableViewController {
         
         cell.favoriteTweet(tweetArray[indexPath.row]["favorited"] as! Bool)
         cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
-        //cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
         return cell
     }
     
